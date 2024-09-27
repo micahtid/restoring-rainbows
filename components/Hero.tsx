c4ce9061c4154a1c96d2d744c49630ab15b2f4d3
Micah Tidball <micahtid@gmail.com>
2024-09-27 17:13:20 +0700

    scalable header component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,26 +42,32 @@ const VerticalGradient: React.FC<VerticalGradientProps> = ({ xPos, className }) 
 
 const Hero = () => {
   return (
-    <section className="w-[100vw] h-[110vh] relative bg-secondary">
+    <section className="w-[100vw] h-[110vh] relative bg-secondary
+    max-lg:h-[600px]">
         <h3
         className="
           dynamic-title text-center text-header drop-shadow mb-52 
-          max-w-[900px] max-md:min-w-[100%] px-4 z-[500]
-          absolute left-1/2 top-[175px] transform -translate-x-1/2"
+          max-w-[900px] max-md:min-w-[100%] px-x z-[500]
+          absolute left-1/2 top-[175px] transform -translate-x-1/2
+          max-lg:left-0 max-lg:-translate-x-0 max-lg:text-left max-lg:top-[50px]"
         >
           Spreading Art and Saving the Planet
         </h3>
-        <div className="absolute w-[45%] left-0 top-[10%]">
-          <VerticalGradient xPos="right-0" />
+        <div className="absolute w-[45%] left-0 top-[10%]
+        max-lg:top-[225px] max-lg:w-[60%]">
+          <VerticalGradient xPos="right-0" className="max-lg:hidden" />
           <img src="/presentation.png" className="w-full h-auto object-cover" />
         </div>
-        <div className="absolute w-[32.5%] left-[45%] bottom-[15%] z-[200]">
-          <HorizontalGradient yPos="top-0" className="-top-[100px]" />
-          <img src="/presentation.png" className="w-full h-auto object-cover" />
-        </div>
-        <div className="absolute w-[22.5%] left-[77.5%] top-[10%]">
-          <VerticalGradient xPos="left-0" className="-left-[100px]" />
+        <div className="absolute w-[32.5%] left-[45%] bottom-[15%] z-[200]
+        max-2xl:bottom-[30%]
+        max-lg:left-[50%] max-lg:w-[45%] max-lg:top-[175px]">
+          <HorizontalGradient yPos="top-0" className="-top-[100px] max-lg:-top-[125px]" />
           <img src="/zoom_meeting.png" className="w-full h-auto object-cover" />
+        </div>
+        <div className="absolute w-[22.5%] left-[77.5%] top-[10%]
+        max-lg:hidden">
+          <VerticalGradient xPos="left-0" className="-left-[100px] max-lg:hidden" />
+          <img src="/splash_five.jpg" className="w-full h-auto object-cover" />
         </div>
     </section>
   );
