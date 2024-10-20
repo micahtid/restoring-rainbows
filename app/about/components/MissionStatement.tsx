f9b6a48df8cba5ffb39efa53c8ac25fbbb2fd387
Micah Tidball <micahtid@gmail.com>
2024-10-20 14:38:33 +0700

    updated about page (added 'our story')

--- app/about/components/MissionStatement.tsx
+++ app/about/components/MissionStatement.tsx
@@ -1,29 +1,51 @@
+import { aboutPageMissionStatement, aboutPageOurStory } from "@/data"
+
 const MissionStatement = () => {
+  const renderParagraphs = (text: string) => {
+    return text.split('\n').map((paragraph, index) => (
+      <p key={index} className="mb-4">{paragraph.trim()}</p>
+    ));
+  };
+
+  const displayData = [
+    {
+      title: "Our Mission",
+      description: aboutPageMissionStatement
+    },
+    {
+      title: "Our Story",
+      description: aboutPageOurStory
+    }
+  ]
+
   return (
     <div className='w-[100vw] mt-24'>
-      <div className="w-full relative overflow-hidden
-      flex justify-start items-center px-x py-24">
-        <img src="/splash_four.png"
-        className='w-full h-full object-cover
-        absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2
-        brightness-75 blur-[2px] z-20'  />
-        <h3 className='z-30 dynamic-subheading
-        text-white max-w-[1000px] py-24
-        fade-in-animation'>
+      <div className="w-full relative overflow-hidden flex justify-start items-center px-x py-24">
+        <img
+          src="/splash_four.png"
+          className='w-full h-full object-cover absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 brightness-75 blur-[2px] z-20'
+        />
+        <h3 className='z-30 dynamic-subheading text-white max-w-[1000px] py-24 fade-in-animation'>
           Championing sustainability and creativity. We turn discarded school supplies into art.
         </h3>
       </div>
-      <div className="max-w-max w-full mx-auto px-x py-8 mt-12
-      flex flex-col justify-start items-start gap-y-12
+
+      <div className="max-w-max w-full mx-auto px-x py-8 mt-12 flex flex-col justify-start items-start gap-y-12 
       fade-in-animation">
-        <div className="flex flex-col gap-y-2">
-          <p className="uppercase font-bold text-primary">Lorem ipsum dolor sit amet.</p>
-          <h3 className='dynamic-heading text-header'>Our Mission</h3>
-        </div>
-        <p className='dynamic-text text-body max-w-[800px]'>Our mission is to give children educational supplies, clean up the environment, and nurture creativity in our community! We do this by collecting new and old school supplies alike and restoring them into powerful instruments of creativity! </p>
+        {
+          displayData.map((data, index) => (
+            <>
+              <h3 className='dynamic-subheading text-header'>{data.title}</h3>
+              <div className='dynamic-text text-body max-w-[1000px]'>
+                {renderParagraphs(data.description)} 
+              </div>
+              <div className="w-full h-[1px] bg-body" />
+            </>
+          ))
+        }
       </div>
     </div>
-  )
+  );
 }
 
-export default MissionStatement
+export default MissionStatement;
--- data/index.tsx
+++ data/index.tsx
@@ -41,27 +41,18 @@ export const socials = [
 import { PiRecycleDuotone, PiChalkboardTeacherDuotone, PiHandHeartDuotone } from "react-icons/pi";
 
 export const whatWeDo = [
-    {icon: <PiRecycleDuotone />, title: "Collect and Refurbish School Supplies", description: "Whether they&apos;re worn out, barely working, or gently used, we collect your school supplies and restore them, keeping them out of landfills."},
+    {icon: <PiRecycleDuotone />, title: "Collect and Refurbish School Supplies", description: "Whether they're worn out, barely working, or gently used, we collect your school supplies and restore them, keeping them out of landfills."},
     {icon: <PiHandHeartDuotone />, title: "Donate School Supplies", description: "We donate school supplies to unlock the creative potential of children, offering them the tools to turn their dreams into colorful realities, no matter their circumstances."},
     {icon: <PiChalkboardTeacherDuotone />, title: "Educate About Climate Change/Art Education", description: "We educate communities about the intersection of climate change and art, raising awareness and inspiring meaningful action."},
 ]
 
 // About Us Page
-export const aboutPageMissionStatement = `Restoring Rainbows is an international, youth-led nonprofit dedicated to making art accessible while protecting the planet. 
-Our mission is to educate and empower diverse communities about the critical connections between art and climate change. With hundreds of volunteers across dozens of countries, 
-we&apos;ve already made a positive impact on countless lives. 
-Restoring Rainbows is a certified 501(c)3 nonprofit organization registered in the United States of America.`
+export const aboutPageMissionStatement = `Restoring Rainbows is an international, youth-led nonprofit dedicated to making art accessible while protecting the planet. Our mission is to educate and empower diverse communities about the critical connections between art and climate change. With hundreds of volunteers across dozens of countries, we've already made a positive impact on countless lives.\n Restoring Rainbows is a certified 501(c)3 nonprofit organization registered in the United States of America.`
 
-export const aboutPageOurStory = `Both Jordan and Grace had a passion for art and recognized the importance and happiness that comes from making art. They lacked the supplies or the resources to make art, 
-but quickly realized this wasn&apos;t the case for everyone with one-third of households in the U.S. not being able to afford basic school supplies, let alone extras like paint or canvases. 
-At the same time, Jordan and Grace were struck by the large quantities of plastic markers and other supplies being discarded in schools—excess on one side, and necessity on the other. 
-As they did more research they became increasingly alarmed by both the lack of access to educational materials, and the global climate crisis. They noticed an opportunity to close the divide 
-by repurposing unused materials and giving them to those who needed them the most. 
-Through Restoring Rainbows Jordan and Grace have personally seen the kindness of certain individuals and the meaningful influence of others, motivating them to keep promoting art accessibility 
-and sustainability globally.`
+export const aboutPageOurStory = `Both Jordan and Grace had a passion for art and recognized the importance and happiness that comes from making art. They lacked the supplies or the resources to make art, but quickly realized this wasn't the case for everyone with one-third of households in the U.S. not being able to afford basic school supplies, let alone extras like paint or canvases. \n At the same time, Jordan and Grace were struck by the large quantities of plastic markers and other supplies being discarded in schools—excess on one side, and necessity on the other. As they did more research they became increasingly alarmed by both the lack of access to educational materials, and the global climate crisis. They noticed an opportunity to close the divide by repurposing unused materials and giving them to those who needed them the most. \nThrough Restoring Rainbows Jordan and Grace have personally seen the kindness of certain individuals and the meaningful influence of others, motivating them to keep promoting art accessibility and sustainability globally.`
 
 export const aboutPageWhyWeMatter = [
-    {number: "700 Million", caption: "Environment", description: "700 million plastic markers are produced annually by the world&apos;s leading marker brand, contributing to the 158,943,925 tons of plastic produced each year!"},
+    {number: "700 Million", caption: "Environment", description: "700 million plastic markers are produced annually by the world's leading marker brand, contributing to the 158,943,925 tons of plastic produced each year!"},
     {number: "33% of Parents", caption: "Cost", description: "Many people struggle to afford educational supplies like markers. In fact, in the United States, 1 in 3 parents expect to go into debt due to Back to School Shopping."},
     {number: "14% Decrease", caption: "Art", description: "A UPenn study found that art exposure in lower socioeconomic neighborhoods was linked to a 14% decrease in cases of child abuse and neglect, 5% decrease in obesity, and an 18% decrease in serious crime rate."},
 ]
@@ -70,4 +61,4 @@ export const aboutPageWhyWeMatter = [
 export const partnerPageDescription = 'Through these collaborative efforts, we aim to inspire change and empower individuals to embrace the arts while supporting environmental responsibility.'
 
 // Start a Branch Page
-export const branchPageDescription = `Interested in starting a branch of Restoring Rainbows? You&apos;ll be joining a network of over 100 branches spanning across more than 40 countries worldwide. Just fill out the form below!`
+export const branchPageDescription = `Interested in starting a branch of Restoring Rainbows? You'll be joining a network of over 100 branches spanning across more than 40 countries worldwide. Just fill out the form below!`
