0995a5cc2e807c1cd6a8d71ffe0d2be33481d77f
Micah Tidball <micahtid@gmail.com>
2024-10-23 23:45:09 +0700

    updated branch page (slightly edited drop down design!)

--- app/branches/components/DropDown.tsx
+++ app/branches/components/DropDown.tsx
@@ -35,17 +35,21 @@ const DropDown: React.FC<DropDownProps> = ({ isOpen, toggleDropdown, cityData })
                 className="absolute z-10 w-[275px] 
                 bg-primary
                 max-h-[300px] overflow-y-auto no-scrollbar 
-                shadow-sm px-4 py-2
-                flex flex-col gap-y-3"
+                shadow-sm
+                flex flex-col"
             >
             {cityData.branches.map((branch: any, branchIndex: number) => (
-                <button
-                    key={branchIndex}
-                    onClick={() => router.push(`/branches/branch?country=${branch.country}&city=${branch.city}&community=${branch.community}`)}
-                    className="cursor-pointer text-white max-[500px]:py-3 text-left"
-                >
-                    {branch.community}
-                </button>
+                <>
+                    <button
+                        key={branchIndex}
+                        onClick={() => router.push(`/branches/branch?country=${branch.country}&city=${branch.city}&community=${branch.community}`)}
+                        className="cursor-pointer text-white max-[500px]:py-3 text-left
+                        px-4 py-2"
+                    >
+                        {branch.community}
+                    </button>
+                    <div className="w-full h-[1px] bg-white" />
+                </>
             ))}
             </ul>
         )
