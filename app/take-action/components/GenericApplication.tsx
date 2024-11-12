3f613b50d6e9af53a39202920a5589e7bf831403
Micah Tidball <micahtid@gmail.com>
2024-11-12 18:25:38 +0700

    general patches [4] (redesigned general application page!)

--- app/take-action/components/GenericApplication.tsx
+++ app/take-action/components/GenericApplication.tsx
@@ -16,25 +16,34 @@ const GenericApplication: React.FC<GenericApplicationProps> = ({ instructions, t
       };
 
   return (
-    <div className='
-    max-w-max mx-auto w-full 
-    px-x pt-40 pb-20
-    flex flex-col gap-y-8'>
-        <h3 className='dynamic-heading text-header'>
-            {title}
-        </h3>
-        <div className="dynamic-text text-body">
-            {renderParagraphs(instructions)}
-        </div>
-        <OutlineButton className="w-[200px] py-2 mt-5
-            bg-primary text-white hover:bg-transparent hover:text-body
-            transition-all duration-500
-            shadow-sm flex justify-center items-center">
-            <a className="dynamic-text font-medium" 
-              href={buttonLink}>
-                {buttonLabel}
-            </a>
-        </OutlineButton>
+    <div className="px-x pt-40 pb-40
+    flex justify-center items-center"
+    style={{
+      backgroundColor: '#d5e3f1',
+      backgroundImage: 'linear-gradient(to top left, #e3f0ff, #e6f4f0)', 
+    }}
+    >
+      <div className='
+      max-w-max mx-auto w-full 
+      flex flex-col gap-y-8 
+      px-6 py-12
+      bg-white drop-shadow-lg'>
+          <h3 className='dynamic-heading text-header'>
+              {title}
+          </h3>
+          <div className="dynamic-text text-body">
+              {renderParagraphs(instructions)}
+          </div>
+          <OutlineButton className="w-[200px] py-2 mt-5
+              bg-primary text-white hover:bg-transparent hover:text-body
+              transition-all duration-500
+              shadow-sm flex justify-center items-center">
+              <a className="dynamic-text font-medium" 
+                href={buttonLink}>
+                  {buttonLabel}
+              </a>
+          </OutlineButton>
+      </div>
     </div>
   )
 }
