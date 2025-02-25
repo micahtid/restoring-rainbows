f0f5710ff284c3c3bb1ee9dbe8ca4d19b6511eb9
Micah Tidball <micahtid@gmail.com>
2025-02-25 09:46:04 +0700

    updated /prism page (added guidelines + submission!)

--- app/prism/page.tsx
+++ app/prism/page.tsx
@@ -10,6 +10,19 @@ import { useData } from "@/providers/useData";
 const Prism = () => {
   const { stories } = useData();
 
+  const prismGuidelines = [
+    { 
+      title: "Prism Guidelines", 
+      description: "Restoring Rainbows requires all digital submissions to abide by a strict set of guidelines. Please review them here, as well as view some helpful submission prompts.",
+      link: "https://docs.google.com/document/d/1Xx0nTrf3jPPodznCGIRYLSMZhrIPjUW04637mogPf_8/edit?usp=sharing"
+    },
+    { 
+      title: "Prism Submission", 
+      description: "Use this form to submit your digital work, and attach a volunteer form for us to sign in order for you to receive your hours.",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSc6zYdvTK5qDpPdRViSAFQ7-HLS5-5qaPhiCCEGCa57W9J6Aw/viewform?usp=sf_link"
+    },
+  ];
+
   if (!stories) {
     return <Loader />;
   }
@@ -23,34 +36,42 @@ const Prism = () => {
     fade-in-animation">
       <Highlight text="Read Our Blogs" />
 
-      <h3 className="dynamic-subheading">
+      <h3 className="dynamic-heading text-header">
         Prism Newsletter
       </h3>
 
-      <p className="dynamic-text text-body">
+      <p className="dynamic-text text-body bg-white/80 backdrop-blur-sm p-4 rounded-lg">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. A omnis, veniam eius sit recusandae, nemo iusto, expedita impedit non aspernatur veritatis unde ipsam officia? Vero ad accusantium repellendus consectetur molestias.
       </p>
 
       <p className="dynamic-subheading mt-20 mb-2">Recent Publications</p>
 
-      <div className="flex flex-col gap-y-4 w-full mb-12">
+      <div className="flex flex-col gap-y-4 w-full mb-12 bg-white/80 backdrop-blur-sm p-6 rounded-lg">
         {displayedStories.map((story, index) => (
-          <>
+          <div key={index} className="group">
             <a 
               className="dynamic-text text-body 
-              hover:text-blue-600 hover:underline"
+              group-hover:text-primary transition-colors duration-300
+              flex items-center gap-x-3"
               href={story.link}>
+              <div className="w-1.5 h-1.5 rounded-full bg-primary/20 group-hover:bg-primary transition-colors duration-300" />
               {story.title}
             </a>
-            <div className="w-full h-[2px] bg-body/20 mb-4" />
-          </>
+            {index !== displayedStories.length - 1 && (
+              <div className="w-full h-[2px] bg-body/20 mt-4" />
+            )}
+          </div>
         ))}
       </div>
 
       <OutlineButton 
         className="w-[150px] py-2 
-        bg-primary/20 shadow-sm
-        flex justify-center items-center"
+        bg-primary text-white
+        transition-all duration-300
+        shadow-sm
+        hover:bg-transparent hover:text-body
+        flex justify-center items-center
+        mb-32"
       >
         <a 
         href="https://prismofficial.substack.com/ " 
@@ -58,6 +79,28 @@ const Prism = () => {
           Subscribe
         </a>
       </OutlineButton>
+
+      {prismGuidelines.map((guide, index) => (
+        <div
+          key={index}
+          className={`flex flex-col mb-32 gap-y-8 w-full ${
+            index % 2 === 0 && "items-end text-right max-md:text-left"
+          }`}
+        >
+          <OutlineButton 
+            className="
+            flex justify-center w-[550px] py-4
+            max-md:w-full
+            bg-primary text-white 
+            transition-all duration-500 
+            shadow-sm text-nowrap 
+            hover:bg-transparent hover:text-body"
+          >
+            <a className="dynamic-subheading max-md:text-lg" href={guide.link}>{guide.title}</a>
+          </OutlineButton>
+          <p className="dynamic-text text-body max-w-[850px]">{guide.description}</p>
+        </div>
+      ))}
     </section>
   );
 }
