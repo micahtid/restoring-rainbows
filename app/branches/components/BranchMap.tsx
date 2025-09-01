9a9045f6566f5b3c778832387229b20fd88e2b6e
Not-Micah <micahtid@gmail.com>
2025-08-31 19:36:26 -0500

    migrated from GMaps to leaflet

--- app/branches/components/BranchMap.tsx
+++ app/branches/components/BranchMap.tsx
@@ -1,23 +1,24 @@
 "use client";
 
-import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { DocumentData } from 'firebase/firestore';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-const containerStyle = {
-  width: '100%',
-  height: '100%',
-};
+// Custom red dot marker
+const redDotIcon = L.divIcon({
+  className: 'custom-red-dot',
+  html: '<div style="background-color: #e53e3e; width: 8px; height: 8px; border-radius: 50%; border: 1px solid white;"></div>',
+  iconSize: [8, 8],
+  iconAnchor: [4, 4],
+});
+
 
 interface BranchMapProps {
   branches: DocumentData[] | null;
 }
 
 const BranchMap: React.FC<BranchMapProps> = ({ branches }) => {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: "AIzaSyDmKrYRHIl-efX8ZoEMgSIJM6jjaQD_-2c"
-  });
-
   const handleMarkerClick = (city: string) => {
     console.log('Clicked City:', city);
   };
@@ -25,21 +26,28 @@ const BranchMap: React.FC<BranchMapProps> = ({ branches }) => {
   return (
     <div className='max-w-max w-full mx-auto mt-10 mb-24 h-[500px] px-4
     fade-in-animation'>
-      {isLoaded && (
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={{ lat: 51.509865, lng: -0.118092 }}
-          zoom={2}
-        >
-          {branches?.map((branch, index) => (
-            <MarkerF
-              key={index}
-              position={{ lat: branch.lat, lng: branch.lng }}
-              onClick={() => handleMarkerClick(branch.city)}
-            />
-          ))}
-        </GoogleMap>
-      )}
+      <MapContainer
+        center={[51.509865, -0.118092]}
+        zoom={2}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        />
+        {branches?.map((branch, index) => (
+          <Marker
+            key={index}
+            position={[branch.lat, branch.lng]}
+            icon={redDotIcon}
+            eventHandlers={{
+              click: () => handleMarkerClick(branch.city)
+            }}
+          >
+            <Popup>{branch.city}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 };
