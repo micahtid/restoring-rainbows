8142b9b5008db7ea51982e825dae7099187fbe1b
Not-Micah <micahtid@gmail.com>
2025-08-31 20:25:24 -0500

    vercel error patch (2)

--- app/branches/components/BranchMap.tsx
+++ app/branches/components/BranchMap.tsx
@@ -86,8 +86,8 @@ const BranchMap: React.FC<BranchMapProps> = ({ branches }) => {
         style={{ height: '100%', width: '100%' }}
       >
         <DynamicTileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         {branches?.map((branch, index) => (
           <DynamicMarker
